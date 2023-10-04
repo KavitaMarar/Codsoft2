cda423f962c87504f895bc28e6df34597d0b1fd5
Kavita Marar <100897926+KavitaMarar@users.noreply.github.com>
2023-10-04 21:48:15 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Interface {
+    public static void main(String[] args) {
+        double initialBalance = 1000;
+
+        ATM atm = new ATM(initialBalance);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                atm.displayMenu();
+                System.out.print("Enter your option: ");
+                int option = scanner.nextInt();
+                atm.processOption(option);
+                System.out.println();
+            }
+        }
+    }
+}
